2bfdbba710b23eff5790e5a7ded95b070d24eb9f
Agamendon <gosha20052811@gmail.com>
2023-08-07 22:34:15 -0400

    first version

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	_ "github.com/joho/godotenv/autoload" // this will register environment variables from .env
@@ -20,7 +22,6 @@ func guildMemberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
 	if err != nil {
 		log.Println("Failed to send new member message: ", err)
 	}
-
 }
 
 func main() {
@@ -29,10 +30,25 @@ func main() {
 		log.Fatal("TOKEN not found in .env, exiting")
 	}
 
-	discord, err := discordgo.New("Bot" + token)
+	discord, err := discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatal("Failed to create session: ", err, ", exiting")
 	}
 
 	discord.AddHandler(guildMemberAdd)
+
+	discord.Identify.Intents = discordgo.IntentsNone
+
+	err = discord.Open()
+	if err != nil {
+		log.Fatal("Error opening connection: ", err)
+	}
+
+	log.Println("App is running. Press CTRL-C or send SIGINT to exit")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	discord.Close()
+
 }
